49541f8e24017bbb4319b6be85326e83bdfbd3a2
olimpiuus <kolegin.yuriu@gmail.com>
2022-03-19 16:10:44 +0200

    burger menu

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu')
+    const burgerButton = document.querySelector('.humburger-menu')
+    const menuLinks = menu.querySelectorAll('.menu-list__link')
+
+    burgerButton.addEventListener('click', () => {
+        menu.classList.add('menu-active')
+    })
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('menu-active')
+        })
+    })
+
+    document.addEventListener('click', (event) => {
+        if (!(event.target.closest('.menu') || event.target.closest('.humburger-menu'))) {
+            menu.classList.remove('menu-active')
+        }
+    })
+})
