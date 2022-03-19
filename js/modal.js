ae075b058daf40c123e1272e39783cc0cc58b246
olimpiuus <kolegin.yuriu@gmail.com>
2022-03-19 16:33:45 +0200

    modal-additional

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const moreButton = document.querySelector('.more')
+    const moreButton = document.querySelectorAll('.more')
     const modal = document.querySelector('.modal')
     const overlay = modal.querySelector('.overlay')
     const modalCloseButton = modal.querySelector('.modal__close')
 
-    moreButton.addEventListener('click', () => {
-        modal.classList.remove('hidden')
+    moreButton.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            modal.classList.remove('hidden')
+        })
     })
+
     overlay.addEventListener('click', () => {
         modal.classList.add('hidden')
     })
